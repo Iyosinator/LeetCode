44911ffb9e019a1d526d62ab62c2a1f9c8b1bb57
Eyosias Mulugeta <119804078+Iyosinator@users.noreply.github.com>
2025-01-30 16:39:13 +0300

    Time: 0 ms (100%), Space: 17.7 MB (34.04%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        res = numBottles
+        num = res
+        while num >= numExchange:
+            rem = num%numExchange
+            free = num//numExchange
+            num = rem+free
+            res+=free
+        return res
+        
