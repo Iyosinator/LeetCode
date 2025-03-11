7d018011d9cdda65914d816d2b311c87197939d9
Eyosias Mulugeta <119804078+Iyosinator@users.noreply.github.com>
2025-03-11 13:26:30 +0300

    Time: 356 ms (50.72%), Space: 17.8 MB (78%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,20 +1,20 @@
 class Solution(object):
-    def threeSumClosest(self, nums, target):
+    def threeSumClosest(self, numbers, target_value):
         
-        nums.sort()
-        closest_sum = float('inf')
+        numbers.sort()
+        nearest_sum = float('inf')
         
-        for i in range(len(nums) - 2):
-            left, right = i + 1, len(nums) - 1
-            while left < right:
-                current_sum = nums[i] + nums[left] + nums[right]
-                if abs(current_sum - target) < abs(closest_sum - target):
-                    closest_sum = current_sum
-                if current_sum < target:
-                    left += 1
-                elif current_sum > target:
-                    right -= 1
+        for index in range(len(numbers) - 2):
+            left_pointer, right_pointer = index + 1, len(numbers) - 1
+            while left_pointer < right_pointer:
+                current_total = numbers[index] + numbers[left_pointer] + numbers[right_pointer]
+                if abs(current_total - target_value) < abs(nearest_sum - target_value):
+                    nearest_sum = current_total
+                if current_total < target_value:
+                    left_pointer += 1
+                elif current_total > target_value:
+                    right_pointer -= 1
                 else:
-                    return current_sum
+                    return current_total
         
-        return closest_sum
+        return nearest_sum
