6a581d082717095658938c6c46a5e626afdbb4e1
Eyosias Mulugeta <119804078+Iyosinator@users.noreply.github.com>
2025-04-03 18:22:13 +0300

    Time: 1304 ms (9.16%), Space: 19 MB (18.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -5,6 +5,7 @@ class Solution:
         unique_number = []
         for i in range(0,n+1):
             expected_array.append(i)
+            
         for y in expected_array:
             if y not in nums:
                 return y
