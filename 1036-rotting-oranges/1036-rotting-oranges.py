c3992feabc3c682a5979b1dd2e15570cc3c6b6ae
Eyosias Mulugeta <119804078+Iyosinator@users.noreply.github.com>
2025-04-18 19:18:53 +0300

    Time: 7 ms (27.14%), Space: 17.8 MB (71.89%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,37 +1,39 @@
-from collections import deque
 from typing import List
+from collections import deque
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         m, n = len(grid), len(grid[0])
         queue = deque()
+        visited = set()
         fresh_count = 0
         minutes = 0
+
+        def inbound(x, y):
+            return 0 <= x < m and 0 <= y < n 
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
                     queue.append((i, j))
+                    visited.add((i, j))
                 elif grid[i][j] == 1:
                     fresh_count += 1
 
         if fresh_count == 0:
             return 0
 
-        def inbound(x, y):
-            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1:
-                    yield nx, ny
-
         while queue:
-            for _ in range(len(queue)):
+            for i in range(len(queue)):
                 x, y = queue.popleft()
-                for nx, ny in inbound(x, y):
-                    grid[nx][ny] = 2
-                    queue.append((nx, ny))
-                    fresh_count -= 1
+                for dx, dy in directions:
+                    nx, ny = x + dx, y + dy
+                    if inbound(nx, ny) and grid[nx][ny] == 1 and (nx, ny) not in visited:
+                        grid[nx][ny] = 2
+                        queue.append((nx, ny))
+                        visited.add((nx, ny)) 
+                        fresh_count -= 1
             minutes += 1
 
         return -1 if fresh_count > 0 else minutes - 1
