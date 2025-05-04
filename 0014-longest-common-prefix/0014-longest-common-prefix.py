31be5b61e19417bb874f74156c2a91c228c3f799
Eyosias Mulugeta <119804078+Iyosinator@users.noreply.github.com>
2025-05-04 19:42:41 +0300

    Time: 0 ms (100%), Space: 18 MB (30.24%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,8 +3,9 @@ class Solution:
         if not strs:
             return ""
         x, y = min(strs), max(strs)
+    
         z = 0
         while z < len(x) and x[z] == y[z]:
             z += 1
-            
+
         return x[:z]
