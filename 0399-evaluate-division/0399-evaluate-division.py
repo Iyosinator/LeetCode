54e21ca91b3f2c211a34fe714ec351896350bb9f
Eyosias Mulugeta <119804078+Iyosinator@users.noreply.github.com>
2025-05-14 15:54:39 +0300

    Time: 0 ms (100%), Space: 17.9 MB (73.33%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -4,33 +4,33 @@ class Solution:
             self.parent = {}
             self.weight = {}
 
-        def find(self, x):
-            if x != self.parent[x]:
-                orig_parent = self.parent[x]
-                self.parent[x] = self.find(orig_parent)
-                self.weight[x] *= self.weight[orig_parent]
-            return self.parent[x]
+        def find(self, node):
+            if node != self.parent[node]:
+                original_parent = self.parent[node]
+                self.parent[node] = self.find(original_parent)
+                self.weight[node] *= self.weight[original_parent]
+            return self.parent[node]
 
-        def union(self, x, y, value):
-            if x not in self.parent:
-                self.parent[x] = x
-                self.weight[x] = 1.0
-            if y not in self.parent:
-                self.parent[y] = y
-                self.weight[y] = 1.0
-            px, py = self.find(x), self.find(y)
-            if px != py:
-                self.parent[px] = py
-                self.weight[px] = value * self.weight[y] / self.weight[x]
+        def union(self, node1, node2, value):
+            if node1 not in self.parent:
+                self.parent[node1] = node1
+                self.weight[node1] = 1.0
+            if node2 not in self.parent:
+                self.parent[node2] = node2
+                self.weight[node2] = 1.0
+            root1, root2 = self.find(node1), self.find(node2)
+            if root1 != root2:
+                self.parent[root1] = root2
+                self.weight[root1] = value * self.weight[node2] / self.weight[node1]
 
-        def isConnected(self, x, y):
-            return x in self.parent and y in self.parent and self.find(x) == self.find(y)
+        def isConnected(self, node1, node2):
+            return node1 in self.parent and node2 in self.parent and self.find(node1) == self.find(node2)
 
-        def getRatio(self, x, y):
-            return self.weight[x] / self.weight[y]
+        def getRatio(self, node1, node2):
+            return self.weight[node1] / self.weight[node2]
 
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         dsu = self.DSU()
-        for (a, b), val in zip(equations, values):
-            dsu.union(a, b, val)
-        return [dsu.getRatio(a, b) if dsu.isConnected(a, b) else -1.0 for a, b in queries]
+        for (var1, var2), val in zip(equations, values):
+            dsu.union(var1, var2, val)
+        return [dsu.getRatio(var1, var2) if dsu.isConnected(var1, var2) else -1.0 for var1, var2 in queries]
