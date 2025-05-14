7595b27065f92ae686740715e1f89dde61adf08c
Eyosias Mulugeta <119804078+Iyosinator@users.noreply.github.com>
2025-05-14 12:29:48 +0300

    Time: 0 ms (100%), Space: 17.9 MB (13.01%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        xor = x ^ y
+        distance = 0
+        while xor:
+            distance += xor & 1
+            xor >>= 1
+        return distance
