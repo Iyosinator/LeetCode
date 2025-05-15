600d107e164ed96853a03041c451bed799bdf727
Eyosias Mulugeta <119804078+Iyosinator@users.noreply.github.com>
2025-05-15 11:55:47 +0300

    Time: 10 ms (41.07%), Space: 18.6 MB (59.47%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -3,7 +3,10 @@ class Solution:
         ans = [0] * (n + 1)
         print(ans)
         for i in range(1, n + 1):
-            ans[i] = ans[i >> 1] + (i & 1)
+            if i%2 == 0:
+                ans[i] = ans[i // 2]
+            else:
+                ans[i] = ans[i // 2] + 1
         return ans
         
 
