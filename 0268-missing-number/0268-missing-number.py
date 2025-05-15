b28fd80f74ac7bdddbf375a10538f02720d1d617
Eyosias Mulugeta <119804078+Iyosinator@users.noreply.github.com>
2025-05-15 12:35:00 +0300

    Time: 3 ms (63.69%), Space: 18.4 MB (98.91%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,11 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        expected_array = []
-        unique_number = []
-        for i in range(0,n+1):
-            expected_array.append(i)
-            
-        for y in expected_array:
-            if y not in nums:
-                return y
+        missing = len(nums)
+        for i, num in enumerate(nums):
+            missing ^= i ^ num
+        return missing
