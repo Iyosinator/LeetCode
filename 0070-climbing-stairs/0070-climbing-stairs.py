a4f1de5b33c89390fa6d11488e3e4caf01c63b3d
Eyosias Mulugeta <119804078+Iyosinator@users.noreply.github.com>
2025-05-19 14:56:28 +0300

    Time: 0 ms (100%), Space: 17.8 MB (30.3%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,19 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # Base cases: if n is 1 or 2, return n directly
-        if n <= 2:
+        if n <= 1:
             return n
         
-        # Initialize DP array to store the number of ways to reach each step
-        dp = [0] * (n + 1)
-        
-        # Base cases: the number of ways to reach step 1 and step 2
+        dp = [0] * (n+1)
+
+        dp[0] = 1
         dp[1] = 1
-        dp[2] = 2
-        
-        # Fill the DP table using the recurrence relation
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        
-        # Return the result for n steps
-        return dp[n]
+
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+
+        return dp[n] 
