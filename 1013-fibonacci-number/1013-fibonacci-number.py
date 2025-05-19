9013b73f01c4d16ffb253c702041a9dc976c971a
Eyosias Mulugeta <119804078+Iyosinator@users.noreply.github.com>
2025-05-19 14:11:23 +0300

    Time: 251 ms (32.14%), Space: 17.9 MB (23.7%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+        if n == 2:
+            return 1
+        else:
+            return self.fib(n-1) + self.fib(n-2)
+            
