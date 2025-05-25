59c5ba1d1906c20eea29ff09e5b34e752cfe5989
Eyosias Mulugeta <119804078+Iyosinator@users.noreply.github.com>
2025-05-25 21:47:30 +0300

    Time: 3 ms (22.46%), Space: 17.8 MB (50.97%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,19 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        brac_dict = {
-            ")":"(",
-            "}":"{",
-            "]":"["
-        }
+        brac_dict = { ")":"(", "}":"{", "]":"[" }
+        opening = set(brac_dict.values())
+
         for char in s:
-            if char in brac_dict:
+            if char in brac_dict.keys():
                 if stack and stack[-1] == brac_dict[char]:
                     stack.pop()
                 else:
                     return False
-            else:
+            elif char in opening:
                 stack.append(char)
+            else:
+                continue
         return not stack            
 
          
