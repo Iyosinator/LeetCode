56646051b9a9752cbecf5bc8430b78571aac0466
Eyosias Mulugeta <119804078+Iyosinator@users.noreply.github.com>
2025-08-08 15:14:31 +0300

    Time: 4 ms (28.99%), Space: 2.2 MB (58.79%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.rs
+++ 0009-palindrome-number/0009-palindrome-number.rs
@@ -1,5 +1,8 @@
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 {
+            return false;
+        }
         let s = x.to_string();
         let rev_s: String = s.chars().rev().collect();
         s == rev_s
