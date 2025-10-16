709936d83bb80c91ade33c37a6363b924ff01669
Mr.Bits <119804078+Iyosinator@users.noreply.github.com>
2025-10-16 11:46:32 +0300

    Time: 181 ms (38.02%), Space: 38.5 MB (48.26%) - LeetHub

---
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import Counter
+
+class Solution:
+    def findSmallestInteger(self, nums: List[int], value: int) -> int:
+        count = Counter(x % value for x in nums)
+        
+        mex = 0
+        while True:
+            r = mex % value
+            if count[r] > 0:
+                count[r] -= 1 
+                mex += 1     
+            else:
+                break           
+        return mex
