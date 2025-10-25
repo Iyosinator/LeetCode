d3295a26e99220405d278b6612934f1cb1fd2af3
Mr.Bits <119804078+Iyosinator@users.noreply.github.com>
2025-10-25 21:29:41 +0300

    Time: 0 ms (100%), Space: 17.7 MB (89.38%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.py
@@ -3,8 +3,9 @@ class Solution:
         full_week = n//7
         left_days = (n-7*(n//7))
 
-        x = (7* (full_week) * (full_week + 7)) / 2
-        y = (left_days * full_week) + (left_days *(left_days + 1)) /2
+        x = (7* (full_week) * (full_week + 7)) / 2 # Full weeks Money
+        y = (left_days * full_week) + (left_days *(left_days + 1)) /2 # Left days Total Money
+
         if n <= 7:
             return int(n *(n+1) /2)
         else:
